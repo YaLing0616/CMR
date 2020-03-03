0b8e4247212d341361eea5cb3f2d86ec6d1b3489
YaLing0616 <s1102b011@gmail.com>
2020-03-03 11:48:56 +0800

    Updates

---
+++ precache-manifest.4b973565b75ed3f39108d6e3241f264c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1e6f275c9f4b2a2ef3f4a126cff3cd",
+    "url": "/CMR/index.html"
+  },
+  {
+    "revision": "4b66ee0940a3ba57a9ef",
+    "url": "/CMR/static/css/main.30ad9131.chunk.css"
+  },
+  {
+    "revision": "6d871a75a386a48ed55e",
+    "url": "/CMR/static/js/2.f2b3f508.chunk.js"
+  },
+  {
+    "revision": "fbfeb1cd824ae9d8bdf8f6749455f732",
+    "url": "/CMR/static/js/2.f2b3f508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b66ee0940a3ba57a9ef",
+    "url": "/CMR/static/js/main.a979ec50.chunk.js"
+  },
+  {
+    "revision": "c5e352653cf4712a5e52",
+    "url": "/CMR/static/js/runtime-main.6a39d6a7.js"
+  },
+  {
+    "revision": "b3fba7ad4272d2c5fab76cbb0a2b07d5",
+    "url": "/CMR/static/media/howtobuymask.b3fba7ad.jpg"
+  },
+  {
+    "revision": "a16fb9ad16ad33e65e7e0e49df3d5b39",
+    "url": "/CMR/static/media/loading.a16fb9ad.svg"
+  },
+  {
+    "revision": "548ab0ebc82994919851c18731e7b21c",
+    "url": "/CMR/static/media/logo.548ab0eb.png"
+  }
+]);
